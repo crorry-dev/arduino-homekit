455401e8b59aa753c015aac16b5e7ab7156ca3c8
crorry-dev <107986905+crorry-dev@users.noreply.github.com>
2022-06-24 18:32:14 +0200

    Update homekit_config.c

--- arduino/homekit_config.c
+++ arduino/homekit_config.c
@@ -1,1 +1,42 @@
+#include <Arduino.h>
+#include <homekit/types.h>
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+#include <stdio.h>
+#include <port.h>
+
+homekit_characteristic_t cha_current_temperature = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
+homekit_characteristic_t cha_humidity  = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            // HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            &cha_current_temperature,
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),//optional
+            NULL
+        }),
+        HOMEKIT_SERVICE(HUMIDITY_SENSOR, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
+            &cha_humidity,
+            NULL
+        }),
+        NULL
+    })
+};
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "123-45-678"
+    //.on_event = on_homekit_event, //optional
+    // .setupId = "1" //optional
+  };
 
