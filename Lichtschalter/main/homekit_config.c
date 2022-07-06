487ac4ab834091945cacb99d1699282da427b83e
crorry-dev <koaxial_pharao_0o@icloud.com>
2022-07-06 22:44:40 +0200

    v1

---
+++ Lichtschalter/main/homekit_config.c
@@ -0,0 +1,43 @@
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+
+
+void my_accessory_identify(homekit_value_t _value) {
+  printf("accessory identify\n");
+}
+
+homekit_characteristic_t cha_current_temperature = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
+homekit_characteristic_t cha_humidity  = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "01234567"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            &cha_current_temperature,
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            NULL
+        }),
+        HOMEKIT_SERVICE(HUMIDITY_SENSOR, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
+            &cha_humidity,
+            NULL
+        }),
+        NULL
+    }),
+    NULL
+};
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "111-11-111"
+    //.on_event = on_homekit_event, //optional
+    // .setupId = "1" //optional
+  };
--- Lichtschalter/main/main.ino
+++ Lichtschalter/main/main.ino
@@ -1,1 +1,107 @@
+#include <Arduino.h>
+#include <arduino_homekit_server.h>;
+#include <ESP8266WiFi.h>
+#include <DHT.h>
 
+#define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
+DHT dht(D3, DHT22);
+
+  
+//access the config defined in C code
+extern "C" homekit_server_config_t config; 
+extern "C" homekit_characteristic_t cha_current_temperature;
+extern "C" homekit_characteristic_t cha_humidity;
+
+const char* ssid = "SSID";
+const char* password = "password";
+
+static uint32_t next_heap_millis = 0;
+static uint32_t next_report_millis = 0;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  wifi_connect();
+  my_homekit_setup();
+}
+
+void loop() {
+  my_homekit_loop();
+  delay(10);
+}
+
+//==============================
+// Homekit setup and loop
+//==============================
+
+void my_homekit_setup() {
+  // homekit_storage_reset();
+  // accessory_init();
+  arduino_homekit_setup(&config);
+}
+
+void my_homekit_loop() {
+  arduino_homekit_loop();
+  const uint32_t t = millis();
+  if (t > next_report_millis) {
+    // report sensor values every 10 seconds
+    next_report_millis = t + 10 * 1000;
+    my_homekit_report();
+  }
+  if (t > next_heap_millis) {
+    // show heap info every 5 seconds
+    next_heap_millis = t + 5 * 1000;
+    // LOG_D("Free heap: %d, HomeKit clients: %d",
+    //     ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+
+  }
+}
+
+void my_homekit_report() {
+  /*
+  float temperature_value = 0.1; 
+  float cha_current_temperature = 0.1;
+  float humidity_value = 0.1;
+  float cha_humidity = 0.1;
+  // LOG_D("Current temperature: %.1f", temperature_value);
+  // LOG_D("Current Humidity: %.1f", humidity_value);
+  homekit_characteristic_notify('0', 0);
+  // homekit_characteristic_notify(1, 1);
+  */
+  
+  float temperature_value = dht.readTemperature();
+  if (isnan(temperature_value)) {
+    cha_current_temperature.value.float_value = 0;
+  }else {
+    cha_current_temperature.value.float_value = temperature_value;
+  }  
+  // cha_current_temperature.value.float_value = temperature_value;
+  float humidity_value = dht.readHumidity();
+  if (isnan(humidity_value)) {
+    cha_humidity.value.float_value = 0;
+  }else {
+    cha_humidity.value.float_value = humidity_value;
+  }
+  // cha_humidity.value.float_value = humidity_value;
+  LOG_D("Current temperature: %.1f", temperature_value);
+  LOG_D("Current Humidity: %.1f", humidity_value);
+  homekit_characteristic_notify(&cha_current_temperature, cha_current_temperature.value);
+  homekit_characteristic_notify(&cha_humidity, cha_humidity.value);
+  
+}
+
+void wifi_connect() {
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.setAutoReconnect(true);
+  WiFi.begin(ssid, password);
+  Serial.println("WiFi connecting...");
+  while (!WiFi.isConnected()) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.print("\n");
+  Serial.printf("WiFi connected, IP: %s\n", WiFi.localIP().toString().c_str());
+}
